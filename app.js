9595643e14d84525548cddb1e6d262ccbb1545e6
ference <fym201@msn.com>
2016-12-08 14:35:17 +0800

    update express to 4.x

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var config = require('./config/admin');
+var bodyParser = require('body-parser');
+var errorhandler = require('errorhandler');
+var methodOverride = require('method-override');
 
 var app = express();
 
@@ -7,31 +10,29 @@ var app = express();
 var pub = __dirname + '/public';
 var view = __dirname + '/views';
 
-app.configure(function() {
-	app.set('view engine', 'html');
-	app.set('views', view);
-	app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
+app.set('views', view);
+app.engine('html', require('ejs').__express);
 
-	app.use(express.methodOverride());
-	app.use(express.bodyParser());
-	app.set('basepath', __dirname);
-});
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('basepath', __dirname);
 
-app.configure('development', function() {
-	app.use(express.static(pub));
-	app.use(express.errorHandler({
+if(process.env.NODE_ENV === 'development'){
+    app.use(express.static(pub));
+	app.use(errorhandler({
 		dumpExceptions: true,
 		showStack: true
 	}));
-});
-
-app.configure('production', function() {
+} else {
 	var oneYear = 31557600000;
 	app.use(express.static(pub, {
 		maxAge: oneYear
 	}));
-	app.use(express.errorHandler());
-});
+	app.use(errorhandler());
+}
+
 
 app.on('error', function(err) {
 	console.error('app on error:' + err.stack);
@@ -46,4 +47,4 @@ app.get('/module/:mname', function(req, resp) {
 });
 
 app.listen(7001);
-console.log('[AdminConsoleStart] visit http://0.0.0.0:7001');
+console.log('[AdminConsoleStart] visit http://localhost:7001');
